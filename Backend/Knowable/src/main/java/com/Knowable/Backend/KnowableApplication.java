857326a4ea31632f21889d3e102d5c6f07522e8c
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2025-07-12 23:52:31 +0530

    spring boot backend configured

---
+++ Backend/Knowable/src/main/java/com/Knowable/Backend/KnowableApplication.java
@@ -0,0 +1,13 @@
+package com.Knowable.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowableApplication.class, args);
+	}
+
+}
---
+++ Backend/Knowable/src/test/java/com/Knowable/Backend/KnowableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Knowable.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
