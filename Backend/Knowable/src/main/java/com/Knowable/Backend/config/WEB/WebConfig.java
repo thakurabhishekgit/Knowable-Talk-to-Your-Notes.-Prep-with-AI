7104b0063898d8aa00a8477711394d26b5530e29
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2025-07-20 19:06:32 +0530

    cors origin

--- Backend/Knowable/src/main/java/com/Knowable/Backend/config/WEB/WebConfig.java
+++ Backend/Knowable/src/main/java/com/Knowable/Backend/config/WEB/WebConfig.java
@@ -2,7 +2,6 @@ package com.Knowable.Backend.config.WEB;
 
 import jakarta.annotation.PostConstruct;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     private final String[] allowedOrigins = {
             "https://6000-firebase-studio-1752484716459.cluster-zumahodzirciuujpqvsniawo3o.cloudworkstations.dev",
             "http://localhost:9002",
+            "https://knowable-ai.vercel.app"
 
     };
 
--- Backend/Knowable/src/main/java/com/Knowable/Backend/dto/UserDTO.java
+++ Backend/Knowable/src/main/java/com/Knowable/Backend/dto/UserDTO.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
